4b9889fe744205fb00569540afb335155a0773fb
Dan-Dongcheol-Lee <leedc123@gmail.com>
2014-09-30 00:02:42 +1000

    Added reconnect logic when it is disconnected from the chat server

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,27 +13,43 @@ angular.module('myApp.controllers', [])
     $scope.chatText = '';
     $scope.messages = [];
 
-    Chat.onopen = function() {
+    Chat.onopen(function() {
+        $scope.$apply(function() {
+            $scope.messages.push({userName: 'System', action: 'info', message: 'Connected', sentDate: new Date().toJSON()});
+            scrollToBottom();
+        });
+
         Chat.registerHandler('spike', function(data) {
             console.log('received a message: ' + JSON.stringify(data));
             $scope.$apply(function() {
                 if (data) {
                     $scope.messages.push(data);
-                    $('#chat-table').animate({ scrollTop: $('#chat-table')[0].scrollHeight}, 100);
+                    scrollToBottom();
                 }
             });
         });
-    };
-    Chat.onclose = function() {
-        console.log("Disconnected from chat");
-    };
-
-//    Chat.close();
+    });
+    Chat.onclose(function() {
+        if ($scope.messages.length === 0 || $scope.messages[$scope.messages.length-1].action !== 'danger') {
+            $scope.$apply(function() {
+                $scope.messages.push({userName: 'System', action: 'danger', message: 'Disconnected', sentDate: new Date().toJSON()});
+                scrollToBottom();
+            });
+        }
+        Chat.reconnect(1000);
+    });
 
     $scope.sendMessage = function() {
         Chat.publish('spike', {userName: $scope.userName, action: 'send', message: $scope.message, sentDate: new Date()});
         $scope.message = '';
     };
+
+    function scrollToBottom() {
+        if ($('#chat-table').height() > 300) {
+            $('#chat-table').height(300);
+        }
+        $('#chat-table').animate({ scrollTop: $('#chat-table')[0].scrollHeight - 100}, 100);
+    }
   }])
   .controller('GameCtrl', ['$scope', function($scope) {
 
--- app/js/services.js
+++ app/js/services.js
@@ -3,8 +3,40 @@
 /* Services */
 
 angular.module('myApp.services', [])
-  .service('Chat', [function() {
-    return new vertx.EventBus('http://localhost:8080/messy-chat');
+  .service('Chat', ['$timeout', function($timeout) {
+    var self = this;
+    self.bus = new vertx.EventBus('http://192.168.0.12:8080/messy-chat');
+    self.openHandler = undefined;
+    self.closeHandler = undefined;
+
+    return {
+        onopen: function(openHandler) {
+            self.bus.onopen = openHandler;
+            self.openHandler = openHandler;
+
+        },
+        registerHandler: function(address, handler) {
+            self.bus.registerHandler(address, handler);
+        },
+        publish: function(address, data) {
+            self.bus.publish(address, data);
+        },
+        onclose: function(closeHandler) {
+            self.bus.onclose = closeHandler;
+            self.closeHandler = closeHandler;
+        },
+        reconnect: function(intervalInMillis) {
+            self.bus = undefined;
+            $timeout(function() {
+                console.log('retry to connect to chat server...');
+                if (!self.bus) {
+                    self.bus = new vertx.EventBus('http://localhost:8080/messy-chat');
+                    self.bus.onopen = self.openHandler;
+                    self.bus.onclose = self.closeHandler;
+                }
+            }, intervalInMillis);
+        }
+    };
   }])
   .service('Currency', ['$resource', function($resource) {
     return $resource('http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json');
